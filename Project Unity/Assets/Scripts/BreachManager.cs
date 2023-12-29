9491931aa14072ca450a488dc5d61f9815f9a3bd
ThatJamGuy <soniccatcraft@gmail.com>
2023-12-28 19:07:40 -0500

    Combined IntroSFX
    Combined them into one audio file so I don't have to sequence them through code, in which also brought changes to the BreachManager script.

--- Project Unity/Assets/Scripts/BreachManager.cs
+++ Project Unity/Assets/Scripts/BreachManager.cs
@@ -4,45 +4,22 @@ using UnityEngine;
 public class BreachManager : MonoBehaviour
 {
     [Header("Audio")]
-    [SerializeField] AudioClip breachStart;
-    [SerializeField] AudioClip alarm;
-    [SerializeField] AudioClip[] bangs;
+    [SerializeField] AudioClip breachSequence;
     [SerializeField] AudioClip[] ambience;
 
     [Header("Audio Sources")]
-    [SerializeField] AudioSource announcementSource;
-    [SerializeField] AudioSource alarmSource;
+    [SerializeField] AudioSource breachSource;
     [SerializeField] AudioSource ambienceSource;
+
+    private void Start()
+    {
+        StartCoroutine(Ambience());
+    }
 
     public void StartBreach()
     {
-        announcementSource.clip = breachStart;
-        alarmSource.clip = alarm;
-
-        alarmSource.Play();
-
-        StartCoroutine(BreachStarterSequence());
-    }
-
-    IEnumerator BreachStarterSequence()
-    {
-        ambienceSource.clip = bangs[0];
-        ambienceSource.Play();
-        yield return new WaitForSeconds(3);
-        announcementSource.Play();
-        yield return new WaitForSeconds(2);
-        ambienceSource.clip = bangs[1];
-        ambienceSource.Play();
-        yield return new WaitForSeconds(5);
-        ambienceSource.clip = bangs[3];
-        ambienceSource.Play();
-        yield return new WaitForSeconds(9);
-        alarmSource.Stop();
-        ambienceSource.clip = bangs[2];
-        ambienceSource.Play();
-
-        yield return new WaitForSeconds(5);
-        StartCoroutine(Ambience());
+        breachSource.clip = breachSequence;
+        breachSource.Play();
     }
 
     IEnumerator Ambience()
