d88c6cea8ceababa5a9f327464f776fab280e5fa
ThatJamGuy <soniccatcraft@gmail.com>
2023-12-30 14:50:07 -0500

    New room + Performance changes

--- Project Unity/Assets/Scripts/MapGen/RoomRenderer.cs
+++ Project Unity/Assets/Scripts/MapGen/RoomRenderer.cs
@@ -1,36 +1,34 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomRenderer : MonoBehaviour
 {
     public float radius = 10f; // Set the radius
+    public LayerMask renderLayer; // Set the layer to filter objects
+
+    private Renderer[] renderers;
+
+    private void Start()
+    {
+        // Find all renderers on the specified layer
+        renderers = FindObjectsOfType<Renderer>();
+    }
 
     private void Update()
     {
-        // Get all game objects in the scene
-        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+        // Find all renderers on the specified layer
+        renderers = FindObjectsOfType<Renderer>();
 
-        foreach (GameObject obj in allObjects)
+        foreach (Renderer renderer in renderers)
         {
-            // Check if the object has a Renderer component
-            Renderer renderer = obj.GetComponent<Renderer>();
-            if (renderer != null)
+            // Check if the object is on the specified layer
+            if (((1 << renderer.gameObject.layer) & renderLayer) != 0)
             {
                 // Calculate the distance between the player and the object
-                float distance = Vector3.Distance(transform.position, obj.transform.position);
+                float distance = Vector3.Distance(transform.position, renderer.transform.position);
 
-                // Check if the object is outside the radius
-                if (distance > radius)
-                {
-                    // If it is, disable the renderer
-                    renderer.enabled = false;
-                }
-                else
-                {
-                    // If it is inside the radius, enable the renderer
-                    renderer.enabled = true;
-                }
+                // Toggle renderer based on distance
+                renderer.enabled = distance <= radius;
             }
         }
     }
