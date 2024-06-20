75f04529a694472b32d6b48d59d78804dd6ed090
ThatJamGuy <soniccatcraft@gmail.com>
2024-06-20 17:50:06 -0400

    Updated Unity to 2022.3.33f1
    Also changes to 173 script

--- Project Unity/Assets/Scripts/NPC/SCP_173.cs
+++ Project Unity/Assets/Scripts/NPC/SCP_173.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SCP_173 : MonoBehaviour
 {
-    
+    [SerializeField] private bool canMove;
+    [SerializeField] private Renderer mesh;
+    private NavMeshAgent agent;
+    private GameObject playerObject;
+
+    private void Start()
+    {     
+        agent = GetComponent<NavMeshAgent>();
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        canMove = true;
+    }
+
+    private void Update()
+    {
+        if(mesh.isVisible)
+            canMove = false;
+        else
+            canMove = true;
+
+        if (canMove)
+        {
+            agent.SetDestination(playerObject.transform.position);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+        }
+    }
 }
