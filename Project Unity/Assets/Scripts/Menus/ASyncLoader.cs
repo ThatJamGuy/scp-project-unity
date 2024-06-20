ea53155a071a17ef21f8546c9ae5842bfb8c0f85
ThatJamGuy <soniccatcraft@gmail.com>
2024-06-20 18:36:06 -0400

    Added ASync loading
    From Start Game to Main Scene. It's really fast loading for me so idk if the screen actually works or not.

---
+++ Project Unity/Assets/Scripts/Menus/ASyncLoader.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ASyncLoader : MonoBehaviour
+{
+    [Header("Menu Screens")]
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject mainMenu;
+
+    [Header("Slider")]
+    [SerializeField] private Slider loadingSlider;
+
+    public void StartGameBtn(string levelToLoad)
+    {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelASync(levelToLoad));
+    }
+
+    IEnumerator LoadLevelASync(string levelToLoad)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingSlider.value = progressValue;
+            Debug.Log(progressValue);
+            yield return null;
+        }
+    }
+}
